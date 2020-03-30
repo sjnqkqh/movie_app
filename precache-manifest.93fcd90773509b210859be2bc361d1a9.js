5d81eae8ec4bbb28f003505981a42ed929f751e8
JunLyul Yang <sjn3649@gmail.com>
2020-03-30 21:06:28 +0900

    Updates

---
+++ precache-manifest.93fcd90773509b210859be2bc361d1a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1bea2343603c4f026080147ae93abb4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "77c7a82b3f15f2287ef1",
+    "url": "/movie_app/static/css/main.6f2d31eb.chunk.css"
+  },
+  {
+    "revision": "697e48abbd7f0ba75f99",
+    "url": "/movie_app/static/js/2.75f33243.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.75f33243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c7a82b3f15f2287ef1",
+    "url": "/movie_app/static/js/main.50311fef.chunk.js"
+  },
+  {
+    "revision": "77df4e0b724824eca1cd",
+    "url": "/movie_app/static/js/runtime-main.0b3e8895.js"
+  }
+]);
